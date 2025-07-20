9618a7ae72fedc0719cf04949aa49479412e4a56
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-07-20 22:32:41 +0200

    feat: Add link to BlogDetailPage

--- src/pages/BlogListPage.jsx
+++ src/pages/BlogListPage.jsx
@@ -4,45 +4,51 @@ import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../config/api";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function BlogListPage() {
+  const [blogList, setBlogList] = useState([]);
+  const { travelId } = useParams();
+  const navigate = useNavigate();
 
-    const [blogList, setBlogList] = useState([])
-    const { travelId } = useParams()
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/blogs.json`)
+      .then((response) => {
+        const blogsArr = Object.keys(response.data).map((id) => ({
+          id,
+          ...response.data[id],
+        }));
 
+        // Filter blogs by travelId from URL
+        const filteredBlogs = blogsArr.filter(
+          (blog) => blog.travelId === travelId
+        );
 
-    useEffect(() => {
-        axios.get(`${BASE_URL}/blogs.json`)
-            .then((response) => {
-                const blogsArr = Object.keys(response.data).map((id) => ({
-                     id,
-                    ...response.data[id],
-                }));
+        setBlogList(filteredBlogs);
+      })
+      .catch((error) =>
+        console.log("Error getting project details from the API...", error)
+      );
+  }, [travelId]);
 
+  const handleBlogClick = (blogId) => {
+    navigate(`/blogs/${blogId}`);
+  };
 
-                // Filter blogs by travelId from URL
-                const filteredBlogs = blogsArr.filter(
-                    (blog) => blog.travelId === travelId
-                );
-
-                setBlogList(filteredBlogs);
-
-            })
-            .catch((error) =>
-                console.log("Error getting project details from the API...", error)
-            );
-    }, [travelId])
-
-    return (
-        <div>
-            <h1>Blogs</h1>
-            {blogList.map((blog) => {
-                return <div key={blog.id}>{blog.title} <img src={blog.imageUrl}></img></div>;
-            })}
-
-        </div>
-    )
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {blogList.map((blog) => {
+        return (
+          <div key={blog.id}>
+            {blog.title}{" "}
+            <img src={blog.imageUrl} onClick={() => handleBlogClick(blog.id)} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default BlogListPage;
