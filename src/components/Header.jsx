8e8fa4c226deda8849bd7077eb0fd6b14b4dbe4e
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-07-21 16:28:06 +0200

    feat: Add styles to Header, NavBar and Imageslider components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,32 +4,22 @@ import logo from "../assets/logo.png";
 function Header() {
   return (
     <header>
-      {/* Top bar: green background, home icon left, buttons right */}
+      {/* Top bar with Home icon on the left and buttons on the right
       <div className="w-full bg-emerald-800 p-4 flex justify-between items-center">
-        {/* Home Icon on the left */}
-        <NavLink
-          to="/"
-          className="flex items-center text-white hover:text-gray-200"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 12l2-2m0 0l7-7 7 7m-9 2v6m0 0h4m-4 0H7"
-            />
-          </svg>
+
+        <NavLink to="/" className="flex items-center text-white hover:text-gray-200">
+          <HomeIcon className="w-6 h-6" />
           <span className="ml-2 hidden sm:inline font-semibold">Home</span>
         </NavLink>
+        </div> */}
 
-        {/* Login and Register buttons on the right */}
-        <div className="flex space-x-2">
+      <div className="w-full bg-emerald-800 p-4 flex justify-end">
+        <div className="flex-1 text-3xl text-stone-300">
+          <NavLink to="/">
+            <i class="fa fa-home"></i>
+          </NavLink>
+        </div>
+        <div className="flex-none">
           <button className="btn btn-neutral bg-stone-300 text-success-content font-semibold">
             Login
           </button>
@@ -39,7 +29,24 @@ function Header() {
         </div>
       </div>
 
-      {/* Rest of your header, like title and quote, goes here */}
+      <NavLink
+        to="/"
+        className="block w-full bg-base-100 p-6 text-3xl font-bold font-sans text-success-content text-center"
+      >
+        <div className="avatar">
+          <div className="w-16 rounded-full">
+            <img src={logo} alt="logo image" />
+          </div>
+        </div>
+        My Travel Palette
+        <div className="text-sm font-normal text-accent-content font-sans">
+          "These are the colors of my heart - the places I've traveled, the
+          skies I've memorized,
+          <br />
+          and the moments that have stayed in my heart. With each trip I paint a
+          little more of my world."
+        </div>
+      </NavLink>
     </header>
   );
 }
