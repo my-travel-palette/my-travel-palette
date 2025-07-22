b1d96a41c8b22edab477577f784bf3b6beccb998
Sevda Kahraman <sevda.s.kahraman@gmail.com>
2025-07-22 12:43:02 +0200

    feat: add Firebase library for auth

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAl81OMUrTuc7ufrT7ffLNO-qxo6hgrw78",
+    authDomain: "my-travel-palette.firebaseapp.com",
+    databaseURL: "https://my-travel-palette-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "my-travel-palette",
+    storageBucket: "my-travel-palette.firebasestorage.app",
+    messagingSenderId: "812088333965",
+    appId: "1:812088333965:web:b6714238b6c4d453e934c4"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
